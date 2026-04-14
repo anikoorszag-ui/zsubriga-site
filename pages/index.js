869c40649b0f3fa40c2150ee9d8f395652b281f7
anikoorszag-ui <aniko.orszag@gmail.com>
2026-04-14 23:13:35 +0200

    Add Home component with company information

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
-
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Zsubriga Kft.</h1>
+      <p>Gyengeáramú rendszerek kivitelezése</p>
+      <p>📞 +36 30 333 9988</p>
+    </div>
+  );
+}
